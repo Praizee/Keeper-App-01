c6cfabda8e03d77d918fee4ef7548ee4a31391a6
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-09 23:24:16 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ function Footer() {
     const currentYear = new Date().getFullYear();
     return (
         <footer>
-            <p>Copyright ⓒ &copy; {currentYear} Stephen Adeniji</p>
+            <p>Copyright ⓒ {currentYear} Stephen Adeniji</p>
+            {/* &copy; */}
         </footer>
     );
 }
