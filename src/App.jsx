5816a6c5f89e9900987a4f45c8ef1d063813e0ff
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-09 22:56:07 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+
+function App() {
+  return (
+
+    <div>
+      <h1>Hello World!</h1>
+      <Footer />
+    </div>
+
+  )
+}
+
+export default App
