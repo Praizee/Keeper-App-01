6955d1b783ef4e3fb1796675230b77709e9f3d1e
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-09 23:19:39 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <header>
-            <h1>Keeper</h1>
+            <a href="#"><h1>Keeper</h1></a>
         </header>
     );
 }
