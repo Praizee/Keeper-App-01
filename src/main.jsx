a1b0ce570639792d2577f40ac0008f922066d158
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-09 23:19:42 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,20 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from "./components/App";
+import '../styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
 // import '../styles.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://w00gz.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
-
+// ReactDOM.render(<App />, document.getElementById("root"));
 
 //1. Create a new React app.
 //2. Create a App.jsx component.
