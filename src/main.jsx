f67139220781dd5cf2d282f30d85b3998079d591
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-14 20:47:40 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from "./components/App";
 import '../styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
 
 
