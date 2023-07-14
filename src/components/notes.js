dc04f48eee560296f713373821cb17d422a4f074
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-14 09:06:13 +0100

    Update notes.js

--- src/components/notes.js
+++ src/components/notes.js
@@ -3,7 +3,7 @@ const notes = [
     key: 1,
     title: "Delegation",
     content:
-      "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+      "Q. How many programmers does it take to change a light bulb?     A. None – It’s a hardware problem"
   },
   {
     key: 2,
