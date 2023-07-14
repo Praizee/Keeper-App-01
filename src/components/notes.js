42db6e64967d976c0adca02bc7c1ba46b7d127a0
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-14 08:36:28 +0100

    Update notes.js

--- src/components/notes.js
+++ src/components/notes.js
@@ -22,5 +22,12 @@ const notes = [
     title: "Hardware vs. Software",
     content:
       "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  },
+  {
+    key: 5,
+    title: "Big ideas",
+    content: "Eat more sushi"
   }
 ];
+
+export default notes;
