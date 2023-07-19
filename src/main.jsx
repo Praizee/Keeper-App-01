8a5759269e25963d7e8ae86e0afa571dd7397ade
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-19 18:56:28 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,18 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
-// Keeper App Part 02
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
+// Keeper App Part 03
+//CHALLENGE:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
+
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
