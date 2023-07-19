1e19166a6607e3e4c1467dbab6cfdd063f8782fe
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-19 17:16:58 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "./notes.js"
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(NoteItem => (
-        <Note
-          key={NoteItem.key}
-          title={NoteItem.title}
-          content={NoteItem.content}
-        />
-      ))}
-
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
