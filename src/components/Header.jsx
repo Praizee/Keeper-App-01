8f3969a1ddaac952a3c714d2b934b23fb9d0aa43
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-19 20:45:37 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
     return (
         <header>
             <a href="#"><h1>Keeper</h1></a>
+            {/* <a href="#"><h1>Keeper App</h1></a> */}
         </header>
     );
 }
