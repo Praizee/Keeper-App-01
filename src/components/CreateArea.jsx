01a2e5f210e3b8d2ef773b2cd7c94011fc2381d3
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-19 20:31:30 +0100

    Update CreateArea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -42,7 +42,8 @@ function CreateArea(props) {
           placeholder="Take a note..."
           rows="3"
         />
-        <button onClick={submitNote}>Add</button>
+        {/* <button onClick={submitNote}>Add</button> */}
+        <button onClick={submitNote}>ADD</button>
       </form>
     </div>
   );
