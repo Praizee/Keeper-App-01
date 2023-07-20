6ff67fe61fc2ed4a438ee5bbbb46ac069ca967e7
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-20 10:34:40 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import HighlightIcon from '@mui/icons-material/Highlight';
 
 function Header() {
     return (
         <header>
-            <a href="#"><h1>Keeper</h1></a>
+            <a href="#"><h1><HighlightIcon />Keeper</h1></a>
             {/* <a href="#"><h1>Keeper App</h1></a> */}
         </header>
     );
