2e48c5d07d000174ad3e46f75a5df28d5e898034
Stephen Adeniji <praiseadeniji2017@gmail.com>
2023-07-20 10:34:42 +0100

    Update CreateArea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: ""
@@ -29,26 +32,34 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand() {
+    setExpanded(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
+        {isExpanded && <input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
-        />
+        />}
         <textarea
           name="content"
+          onClick={expand}
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
         />
-        <Fab onClick={submitNote} className="Fab">
-          {/* <AddCircleOutlineIcon /> */}
-          <AddIcon />
-        </Fab>
+
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote} className="Fab">
+            {/* <AddCircleOutlineIcon /> */}
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
